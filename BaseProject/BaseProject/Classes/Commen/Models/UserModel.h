2cd0895957bb44088091912edfe87c30601701f0
LeoGeng <leo.geng@grapecity.com>
2017-07-23 23:27:27 +0800

    add UserModel

---
+++ BaseProject/BaseProject/Classes/Commen/Models/UserModel.h
@@ -0,0 +1,19 @@
+//
+//  UserModel.h
+//  BaseProject
+//
+//  Created by LeoGeng on 23/07/2017.
+//  Copyright © 2017 cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserModel : NSObject
+@property(retain,atomic) NSString *name;
+@property(retain,atomic) NSString *telNumber;
+@property(retain,atomic) NSString *idNumber;
+@property(retain,atomic) NSString *purchaseName;
+@property(retain,atomic) NSString *purchaseAddress;
+@property(retain,atomic) NSString *purchaseDetailAddress;
+@property(retain,atomic) NSArray *credentialImages;
+@end
---
+++ BaseProject/BaseProject/Classes/Commen/Models/UserModel.m
@@ -0,0 +1,13 @@
+//
+//  UserModel.m
+//  BaseProject
+//
+//  Created by LeoGeng on 23/07/2017.
+//  Copyright © 2017 cc. All rights reserved.
+//
+
+#import "UserModel.h"
+
+@implementation UserModel
+
+@end
