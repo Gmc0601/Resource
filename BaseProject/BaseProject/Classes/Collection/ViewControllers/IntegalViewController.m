063326a2ee81ea2a65a930de4f008eaa1a0353ef
LeoGeng <leo.geng@grapecity.com>
2017-07-29 00:32:46 +0800

    update

--- BaseProject/BaseProject/Classes/Collection/ViewControllers/IntegalViewController.m
+++ BaseProject/BaseProject/Classes/Collection/ViewControllers/IntegalViewController.m
@@ -7,31 +7,90 @@
 //
 
 #import "IntegalViewController.h"
+#import "Constants.h"
+#import "IntegralCell.h"
+#import "IntegralHeaderCell.h"
 
 @interface IntegalViewController ()
-
+@property(retain,atomic) NSArray *models;
 @end
 
 @implementation IntegalViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    UILabel * lab = [[UILabel alloc]init];
+    lab.text = @"积分";
+    lab.textColor = [ColorContants whiteFontColor];
+    lab.font = [UIFont fontWithName:@"PingFang-SC" size:18];
+    [lab sizeToFit];
+    self.navigationItem.titleView = lab;
+    [self addTableView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
 }
 
-/*
-#pragma mark - Navigation
+-(void) addTableView{
+    UITableView *tb = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+    tb.backgroundColor = [UIColor whiteColor];
+    [tb registerClass:[IntegralHeaderCell class] forCellReuseIdentifier:@"cell"];
+    [tb registerClass:[IntegralCell class] forCellReuseIdentifier:@"integralCell.h"];
+    [tb registerClass:[UITableViewHeaderFooterView class] forHeaderFooterViewReuseIdentifier:@"header"];
+    tb.allowsSelection = NO;
+    tb.clipsToBounds=YES;
+    tb.separatorStyle = UITableViewCellSelectionStyleNone;
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    tb.dataSource = self;
+    tb.delegate = self;
+    
+    [self.view addSubview:tb];
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell *cell;
+    
+    if(indexPath.section == 0){
+        cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+        
+    }else{
+        cell = [tableView dequeueReusableCellWithIdentifier:@"integralCell"];
+
+    }
+    
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    if (section == 0) {
+        return 0;
+    }else{
+        return SizeHeight(44);
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.section == 0) {
+        return SizeHeight(262);
+    }else{
+        return SizeHeight(66);
+    }
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    if (section == 0) {
+        return 1;
+    }else{
+        return  _models.count;
+    }
+}
 
 @end
