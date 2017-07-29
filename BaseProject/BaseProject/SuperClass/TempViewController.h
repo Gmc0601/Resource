b2f3101133f640480d86decbf7b1753fd01f335d
LeoGeng <leo.geng@grapecity.com>
2017-07-29 17:50:26 +0800

    add temp root view controller

---
+++ BaseProject/BaseProject/SuperClass/TempViewController.h
@@ -0,0 +1,13 @@
+//
+//  TempViewController.h
+//  BaseProject
+//
+//  Created by LeoGeng on 29/07/2017.
+//  Copyright © 2017 cc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TempViewController : UIViewController
+
+@end
